eb44d934945b54eb5697d2fc536ca70db47921a8
thanhtungpfiev <thanhtungpfiev@gmail.com>
2023-04-20 22:36:05 +0700

    Full Android HeadFirst

---
+++ AndroidHeadFirst/Chapter9/Workout/app/src/main/java/com/example/workout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+        startActivity(intent);
+
+    }
+}
